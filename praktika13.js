16adf6b75ec9aa7df31d1f7570f8f4c5713d32ed
katerina-svaga <ketrin171204@gmail.com>
2026-04-29 23:59:44 +0500

    Add files via upload

---
+++ praktika13.js
@@ -0,0 +1,21 @@
+const IMAGE_SRCS = [
+
+  "https://raw.githubusercontent.com/katerina-svaga/praktoka13/refs/heads/main/f0877d65d844d6a77af7fa129d0ef29b.jpg",
+  "hhttps://raw.githubusercontent.com/katerina-svaga/praktoka13/refs/heads/main/9aa9244985fee4bcf50b37770d79c159_webp.jpg",
+  "https://raw.githubusercontent.com/katerina-svaga/praktoka13/refs/heads/main/fc855426779bdc6a865316a22074cdaa.jpg",
+  "https://raw.githubusercontent.com/katerina-svaga/praktoka13/refs/heads/main/e77c6a0fce97f0dd06503a2cf5edb93c_webp.jpg",
+  "https://raw.githubusercontent.com/katerina-svaga/praktoka13/refs/heads/main/10b239690c2a68a2a1cd917a84b9c7a1.jpg",
+  "https://raw.githubusercontent.com/katerina-svaga/praktoka13/refs/heads/main/5c60fda74f2d9da5e8f45aa84316b4af_webp.jpg"
+
+];
+const IMAGE_ASPECTS = [1, 1, 1, 1, 1, 1];
+const FACE_NAMES = [
+
+  "ХАН 1",
+  "ХАН 2",
+  "ХАН 3",
+  "ХАН 4",
+  "ХАН 5",
+  "ХАН 6"
+
+];
