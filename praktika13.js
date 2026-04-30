916d139d77522c895f2c603f680286254523f471
katerina-svaga <ketrin171204@gmail.com>
2026-04-30 14:27:20 +0500

    Add files via upload

--- praktika13.js
+++ praktika13.js
@@ -1,11 +1,11 @@
 const IMAGE_SRCS = [
 
-  "https://raw.githubusercontent.com/katerina-svaga/praktoka13/refs/heads/main/f0877d65d844d6a77af7fa129d0ef29b.jpg",
-  "hhttps://raw.githubusercontent.com/katerina-svaga/praktoka13/refs/heads/main/9aa9244985fee4bcf50b37770d79c159_webp.jpg",
-  "https://raw.githubusercontent.com/katerina-svaga/praktoka13/refs/heads/main/fc855426779bdc6a865316a22074cdaa.jpg",
-  "https://raw.githubusercontent.com/katerina-svaga/praktoka13/refs/heads/main/e77c6a0fce97f0dd06503a2cf5edb93c_webp.jpg",
+  "https://raw.githubusercontent.com/katerina-svaga/praktoka13/refs/heads/main/5d68cd458742be259d7e975e3e3c216e.jpg",
+  "https://raw.githubusercontent.com/katerina-svaga/praktoka13/refs/heads/main/1aa5de8dfed53225aae18fed10440143.jpg",
+  "https://raw.githubusercontent.com/katerina-svaga/praktoka13/refs/heads/main/5c60fda74f2d9da5e8f45aa84316b4af_webp.jpg",
   "https://raw.githubusercontent.com/katerina-svaga/praktoka13/refs/heads/main/10b239690c2a68a2a1cd917a84b9c7a1.jpg",
-  "https://raw.githubusercontent.com/katerina-svaga/praktoka13/refs/heads/main/5c60fda74f2d9da5e8f45aa84316b4af_webp.jpg"
+  "https://raw.githubusercontent.com/katerina-svaga/praktoka13/refs/heads/main/e77c6a0fce97f0dd06503a2cf5edb93c_webp.jpg",
+  "https://raw.githubusercontent.com/katerina-svaga/praktoka13/refs/heads/main/9aa9244985fee4bcf50b37770d79c159_webp.jpg"
 
 ];
 const IMAGE_ASPECTS = [1, 1, 1, 1, 1, 1];
